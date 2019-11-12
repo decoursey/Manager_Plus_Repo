0a09b50fb3c42072fd735ded00340e88c0b0066a
ddecours <ddecours@uncc.edu>
2019-11-12 14:03:47 -0500

    Added Home, Login, and Signup view controllers

---
+++ managerplusSB/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  managerplusSB
+//
+//  Created by Dylan DeCoursey on 11/9/19.
+//  Copyright © 2019 UNCC Team 9. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
