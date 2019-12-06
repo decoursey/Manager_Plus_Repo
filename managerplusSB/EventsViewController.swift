a89a81f2057b618c8f521740e09c86146a624fd5
ddecours <ddecours@uncc.edu>
2019-12-06 18:19:02 -0500

    printed title to cells

--- managerplusSB/EventsViewController.swift
+++ managerplusSB/EventsViewController.swift
@@ -12,143 +12,179 @@ import AVKit
 import Alamofire
 import SwiftyJSON
 
-class EventsViewController: UIViewController {
-
-
-    @IBOutlet weak var FirstTitleLabel: UILabel!
+class EventsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    let titleArray = ["Title 1", "Title 2"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count}
+                                    
+                                    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "FirstCell", for: indexPath)
+                    cell.textLabel!.text = titleArray[indexPath.row]
+                    return cell}
+    @IBOutlet weak var myTableView: UITableView!
     
     
-    
-    
-    //@IBOutlet weak var FirstLabelUI: UILabel!
-   // var LabelTextST = String()
-    
-    
-    
-    
-    
-    /*
-    protocol ourFunctions {
-        func getTitle(num: Int)
-        func getLocation(num: Int)
-    }
-    */
-
-
-
-
-        
-        
-     
-        
-      /*
-       struct Events: Decodable{
-            let events: EventDetails
-            //let events: Dictionary<String, String>
-        }
-
-        struct EventDetails: Decodable{
-            let eventCont: [EventCont]
-            //let time:
-        }
-
-
-
-
-        struct EventCont: Decodable{
-            let title: String?
-            let locationName: String?
-        }
-
-        struct EventInst: Decodable{
-            let eventInst: [EventInstances]
-        }
-
-        struct EventInstances: Decodable{
-            let start: String?
-        }
-
-    */
-
-
-
             override func viewDidLoad() {
                 super.viewDidLoad()
                 
-                
-               
-                
-                
-                // Do any additional setup after loading the view.
-                /*
-                func stringToDate(date: String){
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-                    dateFormatter.timeZone = TimeZone(abbreviation: "GMT+0:00")
-                    _ = dateFormatter.date(from: date)
-                }
-*/
-                
-                
-                let jsonURLString = "https://campusevents.uncc.edu/api/2/events"
-                guard let url = URL(string: jsonURLString) else
-                { return }
+                myTableView.delegate = self
+                myTableView.dataSource = self
+                                
+                 let jsonURLString = "https://campusevents.uncc.edu/api/2/events"
+                     guard let url = URL(string: jsonURLString) else
+                     { return }
 
-                Alamofire.request(url, method: .get).responseJSON(completionHandler: { (response) in
-                    if response.result.isSuccess {
-                        let data = JSON(response.result.value!)
-                        
-                        let events = data["events"].arrayValue
-                        
-                        var eventData: JSON?
-                        //var eventTitle: String?
-                        
-                    
-                        
-                        
-                        var eventTitles: [String] = []
-                        for event in events {
-                            eventData = event["event"]
-                            eventTitles.append(((eventData?["title"].stringValue)!))
+                     Alamofire.request(url, method: .get).responseJSON(completionHandler: { (response) in
+                         if response.result.isSuccess {
+                             let data = JSON(response.result.value!)
+                             
+                             let events = data["events"].arrayValue
+                             
+                             var eventData: JSON?
+                             //var eventTitle: String?
+                             
+                         
+                             
+                             
+                             var eventTitles: [String] = []
+                             for event in events {
+                                 eventData = event["event"]
+                                 eventTitles.append(((eventData?["title"].stringValue)!))
+                                 
+                             }
+                             //print(eventTitles[1])
+                             //self.FirstTitleLabel.text = eventTitles[1]
+                            // var t1: String
+                             //t1 = eventTitles[1]
+                             //self.titleArray.append(t1)
+
+                             //self.FirstLabelUI.text = eventTitles[1]
+                             //func numberOfSections(in tableView: UITableView) -> Int {
+                                 //return 1
+                             //}
+                             
+                                 var eventLoc: [String] = []
+                                 for event in events {
+                                 eventData = event["event"]
+                                 eventLoc.append(((eventData?["location_name"].stringValue)!))
+                             }
                             
-                        }
-                        //print(eventTitles[1])
-                        self.FirstTitleLabel.text = eventTitles[1]
-                        
-                        //self.FirstLabelUI.text = eventTitles[1]
-                        
-                        
-                        
-                        
-                            var eventLoc: [String] = []
-                            for event in events {
-                            eventData = event["event"]
-                            eventLoc.append(((eventData?["location_name"].stringValue)!))
-                        }
-                       
-                    
-                      /*
-                       func getDate(num: Int)-> String{
-                           var eventDate: [String] = []
-                           for event in events {
-                            eventData = event["event"]
-                           eventInst = eventData["event_instance"]
-                           eventDate.append(((eventInst?["location_name"].stringValue)!))
-                           
-                       }
-                           return eventInst[num]
-                       }
-                        
-                        */
-                        
-                        
-                        
-                    } else {
-                        print("Error")
-                    }
-                    
+                         
+                           /*
+                            func getDate(num: Int)-> String{
+                                var eventDate: [String] = []
+                                for event in events {
+                                 eventData = event["event"]
+                                eventInst = eventData["event_instance"]
+                                eventDate.append(((eventInst?["location_name"].stringValue)!))
+                                
+                            }
+                                return eventInst[num]
+                            }
+                             
+                             */
+                 
+                             
+                         } else {
+                             print("Error")
+                         }
                 })
-                
+        }
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+ //@IBOutlet weak var FirstTitleLabel: UILabel!
+ 
+ 
+ 
+ 
+ //@IBOutlet weak var FirstLabelUI: UILabel!
+// var LabelTextST = String()
+ 
+ 
+ 
+ 
+ 
+ /*
+ protocol ourFunctions {
+     func getTitle(num: Int)
+     func getLocation(num: Int)
+ }
+ */
+
+
+
+
+     
+     
+  
+     
+   /*
+    struct Events: Decodable{
+         let events: EventDetails
+         //let events: Dictionary<String, String>
+     }
+
+     struct EventDetails: Decodable{
+         let eventCont: [EventCont]
+         //let time:
+     }
+
+
+
+
+     struct EventCont: Decodable{
+         let title: String?
+         let locationName: String?
+     }
+
+     struct EventInst: Decodable{
+         let eventInst: [EventInstances]
+     }
+
+     struct EventInstances: Decodable{
+         let start: String?
+     }
+
+ */
+
+ //var titleArray : [String] = []
+
     //            URLSession.shared.dataTask(with: url) { (data, response, err) in
     //
     //                guard let data = data else { return }
@@ -167,24 +203,22 @@ class EventsViewController: UIViewController {
     ////                }
     //
     //                }.resume()
-            }
+            
 
 
 
 
 
-    }
     
-    //self.delegate?.getTitle(0)
-    //self.delegate?
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 
+
+                /*
+                func stringToDate(date: String){
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+                    dateFormatter.timeZone = TimeZone(abbreviation: "GMT+0:00")
+                    _ = dateFormatter.date(from: date)
+                }
+*/
